b45487ffb13e5063c3af66a8d1c16cf9ec25b071
k2036328 <77748869+k2036328@users.noreply.github.com>
2021-04-15 05:33:01 +0100

    Delete Promotions.php

--- Downloads/DatabaseCoursework-master-ad63be6f33528b2af75010911fb6d26b88086b0d/Jeongho/Promotions.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-    <head>
-        <title>Promotions page</title>
-    </head>
-    <body background="background6.jpg">
-        <p>Hello Promotion!</p>
-    </body>
-</html>
